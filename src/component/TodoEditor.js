6318609459b193ca535ed437a53b30d2d1076c62
Jangwoo Park <p.jangwoo@gmail.com>
2023-08-21 17:19:28 +0900

    8월 21일 - 5시

---
+++ src/component/TodoEditor.js
@@ -0,0 +1,17 @@
+import './TodoEditor.css'; 
+
+const TodoEditor = () => {
+
+    return (
+        <div className = "TodoEditor"> 
+            <h4> 새로운 todo 작성하기 ⛷  </h4>
+            <div className ="editor_wrapper">
+                <input  placeholder ="새로운 todo..." />
+                <button> 추가 </button>
+            </div>
+
+        </div>
+    ); 
+
+}
+export default TodoEditor; 
---
+++ src/component/TodoList.js
@@ -0,0 +1,25 @@
+import TodoItem from "./TodoItem";
+import './TodoList.css'; 
+
+const TodoList = ({todo, onUpdate, onDelete}) => {
+
+    return (
+    <div className ="TodoList">
+        <h4> Todo List 🧞‍♂️🧞‍♀️</h4>
+        <input 
+        className ="searchbar"
+        placeholder="검색어를 입력하세요" /> 
+
+        <div className = "list_wrapper">
+            
+           <TodoItem /> 
+
+        </div>
+
+
+
+    </div>
+    );
+}
+
+export default TodoList; 
